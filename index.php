f5b8ea58bb2156ec812746ca80ea1706db4a39f7
Egor Kitselyuk <ekitselyuk@ronasit.com>
2016-08-06 20:49:40 +0600

    add Api

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-header('Location: https://telegram.me/ronasit_lunchbot');
+
+require_once "vendor/autoload.php";
+
+try {
+    $bot = new \TelegramBot\Api\Client('237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw');
+    $bot->command('start', function ($message) use ($bot) {
+
+        $bot->sendMessage($message->getChat()->getId(), "Start!");
+    });
+    $bot->command('stop', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(), "Stop!");
+    });
+    $bot->run();
+} catch (\TelegramBot\Api\Exception $e) {
+    $e->getMessage();
+}
--- webhook.php
+++ webhook.php
@@ -2,28 +2,24 @@
 /**
  * Telegram Bot access token and API url.
  */
+
+use TelegramBot\Api\Client;
+
 $access_token = '237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw';
 $api = 'https://api.telegram.org/bot' . $access_token;
-/**
- * Listen commands, set chat ID and message.
- */
-$output = json_decode(file_get_contents('php://input'), TRUE);
-$chat_id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
-/**
- * Set response.
- */
-switch($message) {
-    case '/start':
-        sendMessage($chat_id, "Done!");
-        break;
-    default:
-        sendMessage($chat_id, "Ups!");
-        break;
-}
-/**
- * Function sendMessage().
- */
-function sendMessage($chat_id, $message) {
-    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+
+
+try {
+    $bot = new Client('YOUR_BOT_API_TOKEN', 'YOUR_BOTAN_TRACKER_API_KEY');
+    $bot->command('devanswer', function ($message) use ($bot) {
+        preg_match_all('/{"text":"(.*?)",/s', file_get_contents('http://devanswers.ru/'), $result);
+        $bot->sendMessage($message->getChat()->getId(),
+            str_replace("<br/>", "composer somecommand\n", json_decode('"' . $result[1][0] . '"')));
+    });
+    $bot->command('qaanswer', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(), file_get_contents('http://qaanswers.ru/qwe.php'));
+    });
+    $bot->run();
+} catch (\TelegramBot\Api\Exception $e) {
+    $e->getMessage();
 }
