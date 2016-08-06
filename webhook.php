21014a7a0378f9884ebe0b69f9166d595da9f631
Egor Kitselyuk <ekitselyuk@ronasit.com>
2016-08-06 21:27:12 +0600

    update api

--- webhook.php
+++ webhook.php
@@ -2,24 +2,33 @@
 /**
  * Telegram Bot access token and API url.
  */
-
-use TelegramBot\Api\Client;
-
 $access_token = '237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw';
 $api = 'https://api.telegram.org/bot' . $access_token;
-
-
-try {
-    $bot = new Client('YOUR_BOT_API_TOKEN', 'YOUR_BOTAN_TRACKER_API_KEY');
-    $bot->command('devanswer', function ($message) use ($bot) {
-        preg_match_all('/{"text":"(.*?)",/s', file_get_contents('http://devanswers.ru/'), $result);
-        $bot->sendMessage($message->getChat()->getId(),
-            str_replace("<br/>", "composer somecommand\n", json_decode('"' . $result[1][0] . '"')));
-    });
-    $bot->command('qaanswer', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), file_get_contents('http://qaanswers.ru/qwe.php'));
-    });
-    $bot->run();
-} catch (\TelegramBot\Api\Exception $e) {
-    $e->getMessage();
+/**
+ * Listen commands, set chat ID and message.
+ */
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$message = $output['message']['text'];
+/**
+ * Set response.
+ */
+switch($message) {
+<<<<<<< HEAD
+=======
+    // Weather API by OpenWeatherMap.
+    // @see http://api.openweathermap.org
+>>>>>>> development
+    case '/start':
+        sendMessage($chat_id, "Done!");
+        break;
+    default:
+        sendMessage($chat_id, "Ups!");
+        break;
+}
+/**
+ * Function sendMessage().
+ */
+function sendMessage($chat_id, $message) {
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
 }
