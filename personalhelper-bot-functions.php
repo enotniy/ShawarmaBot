c8e240d2e04a7ea20155fe9dd173daa9cb399589
Egor Kitselyuk <ekitselyuk@ronasit.com>
2016-08-06 20:15:59 +0600

    init

---
+++ personalhelper-bot-functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Telegram Bot access token and API url.
+ */
+$access_token = '237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw';
+$api = 'https://api.telegram.org/bot' . $access_token;
+/**
+ * Listen commands, set chat ID and message.
+ */
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$message = $output['message']['text'];
+/**
+ * Set response.
+ */
+switch($message) {
+<<<<<<< HEAD
+=======
+    // Weather API by OpenWeatherMap.
+    // @see http://api.openweathermap.org
+>>>>>>> development
+    case '/start':
+        sendMessage($chat_id, "Done!");
+        break;
+    default:
+        sendMessage($chat_id, "Ups!");
+        break;
+}
+/**
+ * Function sendMessage().
+ */
+function sendMessage($chat_id, $message) {
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
