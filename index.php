092b82de5f5d3068758ab0f63f6a638c8b178ba9
SAYGAN <sergey.saygan@gmail.com>
2016-08-06 22:42:26 +0600

    odindvatri

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once "vendor/autoload.php";
+use \TelegramBot\Api\Client;
 
 try {
-    $bot = new \TelegramBot\Api\Client('237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw');
+    //$bot = new \TelegramBot\Api\Client('237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw');
+    $bot = new Client('237071178:AAFpXWKXM2K5MQZwNYmkWAFP6G1bq3KQeEw');
     $bot->command('start', function ($message) use ($bot) {
 
         $bot->sendMessage($message->getChat()->getId(), "Start!");
