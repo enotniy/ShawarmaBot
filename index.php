020ca35108749e5217e473a58386f95d22bd0fd5
Egor Kitselyuk <ekitselyuk@ronasit.com>
2016-08-08 12:43:36 +0600

    test

--- index.php
+++ index.php
@@ -9,22 +9,14 @@ try {
 
     $bot->inlineQuery(function (\TelegramBot\Api\Types\Inline\InlineQuery $inlineQuery) use ($bot) {
         /* @var \TelegramBot\Api\BotApi $bot */
-        $html = file_get_contents('http://stavklass.ru/images/search?utf8=✓&image[text]=' . $inlineQuery->getQuery());
-        $doc = new DOMDocument();
-        $doc->loadHTML($html);
-        $xpath = new DOMXPath($doc);
-        $img = $xpath->query('//div[@class="image-content"]/a[@class="image"]/img');
-        $result = [];
-        for ($i = 0; $i < $img->length; $i++) {
-            /* @var \DOMNodeList $img */
-            $node = $img->item($i);
-            $url = $node->getAttribute('src');
-            list($width, $height) = getimagesize($url);
-            $result[] = new \TelegramBot\Api\Types\Inline\QueryResult\Photo(
-                hash("md5", $url), $url, $url, $url, 'image/jpeg', $width, $height
-            );
-        }
-        $bot->answerInlineQuery($inlineQuery->getId(), $result, 0);
+
+
+        $result = new TelegramBot\Api\Types\Inline\QueryResult\Photo(
+            1,
+            "http://2.bp.blogspot.com/-_1K702HuofI/Vb-2p8r73LI/AAAAAAAAR5s/UFxWoCZGTR8/s1600/Shawarma-Food-Pakistan.jpg",
+            "http://2.bp.blogspot.com/-_1K702HuofI/Vb-2p8r73LI/AAAAAAAAR5s/UFxWoCZGTR8/s1600/Shawarma-Food-Pakistan.jpg");
+
+        $bot->answerInlineQuery($inlineQuery->getId(), [$result], 0);
     });
 
     $bot->run();
